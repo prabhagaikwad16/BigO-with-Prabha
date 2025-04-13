b86bc7fd0daf7e81aac378c02ad7b6605b054a0a
Prabha Gaikwad <prabhagaikwad16@gmail.com>
2025-04-13 13:13:45 +0530

    Detect LinkList cycle.

---
+++ src/b_LinkedList/d_LinkedList_Cycle.java
@@ -0,0 +1,68 @@
+package b_LinkedList;
+
+/*Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. Otherwise, return false.
+
+
+
+Example 1:
+
+
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+Example 2:
+
+
+Input: head = [1,2], pos = 0
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+Example 3:
+
+
+Input: head = [1], pos = -1
+Output: false
+Explanation: There is no cycle in the linked list.
+
+
+Constraints:
+
+The number of the nodes in the list is in the range [0, 104].
+-105 <= Node.val <= 105
+pos is -1 or a valid index in the linked-list.*/
+public class d_LinkedList_Cycle {
+
+    public static void main(String[] args) {
+        /*
+        head-> 1 -> 2 -> 3 -> 4
+                    ^         |
+                    |_________|
+        * */
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = head.next;
+
+        d_LinkedList_Cycle linkedListCycle = new d_LinkedList_Cycle();
+        boolean hasCycle = linkedListCycle.hasCycle(head);
+        System.out.println("Has cycle: " + hasCycle);
+    }
+
+    public boolean hasCycle(ListNode head) {
+        // Use two pointers (slow and fast) to detect a cycle in the linked list
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true; // Cycle detected
+            }
+        }
+        return false; // No cycle
+    }
+}
