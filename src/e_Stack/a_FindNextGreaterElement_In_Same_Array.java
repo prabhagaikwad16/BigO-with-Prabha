29e7c968217ec033638e05d5d85c80538162826f
Prabha Gaikwad <prabhagaikwad16@gmail.com>
2025-04-20 01:21:20 +0530

    Next greater element in same array

--- src/e_Stack/a_FindNextGreaterElement_In_Same_Array.java
+++ src/e_Stack/a_FindNextGreaterElement_In_Same_Array.java
@@ -18,9 +18,13 @@ public class a_FindNextGreaterElement_In_Same_Array {
     //nums2 = [1,3,4,2]
     public int[] nextGreaterElement(int[] arr) {
 
+        /*We use a stack to remember the numbers we've already seen while going from right to left.
+        It helps us quickly check what number might be the next bigger number for the current element.*/
         Stack<Integer> stack = new Stack<>();
         int result[] = new int[arr.length];
 
+        /*So if you're looking for what's on the right, then naturally you need to start checking from the end — because that’s where the rightmost elements are.
+        This way, for each number, you're looking at the things that come after it, not before it.*/
         for(int i = arr.length - 1; i >= 0; i--) {
             while(!stack.isEmpty() && arr[i]>stack.peek()){
                 stack.pop();
