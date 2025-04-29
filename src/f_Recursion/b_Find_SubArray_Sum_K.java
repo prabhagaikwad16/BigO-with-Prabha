a24237ab793e31eac9b130724bc91ef2e81d41c7
Prabha Gaikwad <prabhagaikwad16@gmail.com>
2025-04-29 21:59:51 +0530

    find sub array

---
+++ src/f_Recursion/b_Find_SubArray_Sum_K.java
@@ -0,0 +1,43 @@
+package f_Recursion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class b_Find_SubArray_Sum_K {
+
+    public static void main(String[] args) {
+
+        int arr[] = {1,1,1};
+        int target = 2;
+        b_Find_SubArray_Sum_K findSubArraySumK = new b_Find_SubArray_Sum_K();
+        int result = findSubArraySumK.subarraySum(arr,target);
+        System.out.println("Total subarrays with sum " + target + " = " + result);
+    }
+
+    public int subarraySum(int arr[],int expectedSum){
+        List<List<Integer>> result = new ArrayList<>();
+        findSubArraysWithSumK(0,arr,new ArrayList<>(),0,expectedSum,result);
+        for(List list: result){
+            System.out.println(list);
+        }
+        return result.size();
+    }
+
+    private void findSubArraysWithSumK(int index, int arr[], List<Integer> subArray,int currentSum, int target, List<List<Integer>> result){
+
+        if(index == arr.length){
+            if(currentSum == target){
+                result.add(new ArrayList<>(subArray));
+            }
+            return;
+        }
+
+
+        subArray.add(arr[index]);
+        findSubArraysWithSumK(index+1, arr, subArray, currentSum + arr[index], target, result);
+
+        subArray.remove(subArray.size()-1);
+        findSubArraysWithSumK(index+1, arr, subArray, currentSum, target, result); //Exclude current element (arr[index])
+    }
+
+}
