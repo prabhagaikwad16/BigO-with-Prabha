946f725c3c50629a8d8088ddb2074eedabc9cd7d
Prabha Gaikwad <prabhagaikwad16@gmail.com>
2025-05-04 12:15:14 +0530

    Lemondate change greedy approach

--- src/g_greedy/a_AssignCookies.java
+++ src/g_greedy/a_AssignCookies.java
@@ -1,2 +1,35 @@
-package g_greedy;public class a_AssignCookies {
+package g_greedy;
+
+import java.util.Arrays;
+
+public class a_AssignCookies {
+
+    public static void main(String[] args) {
+
+        int g[] = {1,2,3};
+        int s[] = {1,1};
+
+        a_AssignCookies aAssignCookies = new a_AssignCookies();
+        int result = aAssignCookies.findContentChildren(g, s);
+        System.out.println("Result = "+result);
+    }
+
+    public int findContentChildren(int[] g, int[] s) {
+
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int i = 0;
+        int j = 0;
+        while(i<g.length && j<s.length){
+
+            if(g[i]<=s[j]){
+                i++;
+                j++;
+            }else{
+                j++;
+            }
+        }
+        return i;
+    }
 }
---
+++ src/g_greedy/b_LemonadeChange.java
@@ -0,0 +1,47 @@
+package g_greedy;
+
+public class b_LemonadeChange {
+    public static void main(String[] args) {
+
+        int bills[] = {5,5,10,10,20};
+
+        b_LemonadeChange lemonadeChange = new b_LemonadeChange();
+        boolean result = lemonadeChange.lemonadeChange(bills);
+        System.out.println("Result = "+result);
+    }
+
+
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0;
+        int ten = 0;
+
+        for(int i=0; i<bills.length; i++){
+            if(bills[i] == 5){
+                five++;
+            }
+            else if(bills[i] == 10){
+                if(five>0){
+                    ten++;
+                    five--;
+                }else{
+                    return false;
+                }
+            }
+            else if(bills[i] == 20){
+                if(ten>0 && five>0){
+                    ten--;
+                    five--;
+                }else if(five>=3){
+                    five = five-3;
+                }
+                else{
+                    return false;
+                }
+            }else{
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
